d9f660794b621636c23866f5d9379b7da0a2177a
xuxiaoming <1229717912@qq.com>
2019-03-10 22:51:32 +0800

    第一个图表

--- mpl_squares.py
+++ mpl_squares.py
@@ -6,4 +6,10 @@ import matplotlib.pyplot as plt
 squares = [1,4,9,16,25]
 
 plt.plot(squares)
+plt.title("Squares Number")
+plt.xlabel("value",fontsize = 14)
+plt.ylabel("Square of Value",fontsize = 14)
+# 设置刻度标记的大小
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
+
