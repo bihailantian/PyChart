73cd90db102d03c6495e11282a6a58c5242583d3
xuxiaoming <1229717912@qq.com>
2019-04-09 21:31:35 +0800

    给点着色

--- rw_visual.py
+++ rw_visual.py
@@ -6,13 +6,18 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
+
 # 只要程序处于活动状态，就不断地模拟随机漫步
 while True:
     # 创建一个RandomWalk实例，并将其包含的点都绘制出来
     rw = RandomWalk()
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = list(range(rw.num_points))
+    #plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,edgecolor='none', s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolor='none', s=1)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n):")
