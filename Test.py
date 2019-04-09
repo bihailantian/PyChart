d28376a5b06d6302bc5e5a2a599163fc0599c162
xuxiaoming <1229717912@qq.com>
2019-04-09 21:39:33 +0800

    突出起点和终点

---
+++ Test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+# python列表获取元素最后一个值的方法
+list1 = [1, 2, 3, 4, 5]
+
+print(list1[-1])
--- rw_visual.py
+++ rw_visual.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-
 # 只要程序处于活动状态，就不断地模拟随机漫步
 while True:
     # 创建一个RandomWalk实例，并将其包含的点都绘制出来
@@ -14,9 +13,13 @@ while True:
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
-    #plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,edgecolor='none', s=15)
+    # plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,edgecolor='none', s=15)
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-                edgecolor='none', s=1)
+                edgecolor='none', s=15)
+
+    # 突出起点和终点
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
     plt.show()
 
