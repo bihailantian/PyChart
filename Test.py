76e0f13d67425ac7ffbd1c5752c948fd5fab2516
xuxiaoming <1229717912@qq.com>
2019-04-17 21:48:57 +0800

    添加日期

--- Test.py
+++ Test.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+from datetime import datetime
+
+
 # python列表获取元素最后一个值的方法
 list1 = [1, 2, 3, 4, 5]
 
 print(list1[-1])
+
+
+
+first_date = datetime.strptime("2014-07-01","%Y-%m-%d")
+first_date2 = datetime.strptime("2014-07-01","%y")
+print(first_date)
+print(first_date2)
--- highs_lows.py
+++ highs_lows.py
@@ -4,6 +4,12 @@
 import csv
 
 import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+first_date = datetime.strptime("2014-07-01","%Y-%m-%d")
+print(first_date)
+
 
 # 读取文件头
 filename = 'sitka_weather_07-2014.csv'
@@ -17,20 +23,24 @@ with open(filename) as f:
         print(index, column_header)
 
     # 打印每一天的最高温
-    highs = []
+    dates,highs = [],[]
     for row in reader:
+        current_date = datetime.strptime(row[0],'%Y-%m-%d')
+        dates.append(current_date)
         high = int(row[1])
         highs.append(high)
+
     print(highs)
 
     # 设置绘图窗口的尺寸
     fig = plt.figure(dpi=128, figsize=(10, 6))
 
-    plt.plot(highs, c='red')
+    plt.plot(dates,highs, c='red')
 
     # 设置图形的格式
     plt.title("Daily high temperatures, July 2014", fontsize=24)
     plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
 
